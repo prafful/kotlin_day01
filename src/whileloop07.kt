be67046ae94c9935dd375833a54e1869b88aa315
Prafful Daga <prraful@gmail.com>
2025-02-09 11:24:38 +0800

    Add files via upload

---
+++ src/whileloop07.kt
@@ -0,0 +1,13 @@
+/*
+    while(testexpression){
+        //this block keeps on executing repeatedly till testexpression return true
+    }
+*/
+fun main() {
+    var n1 = 0
+    while(n1<6){
+        println(n1)
+        n1+=1 //n1 = n1 + 1
+    }
+    println("outside the loop!!!!")
+}
