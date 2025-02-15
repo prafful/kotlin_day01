0df1ef30b58567d89bfda212fe0100e60ca82ba3
Prafful Daga <prraful@gmail.com>
2025-02-15 10:27:43 +0800

    Add files via upload

--- src/oopconstructor.kt
+++ src/oopconstructor.kt
@@ -1,14 +1,15 @@
 //class in object oriented programming
 
 //create a class using class keyword
-class Car{
+//define constructor by making a Car class receive some parameters
+class CarModel(color:String){
 
     /*
         by default all class variables and methods are public
      */
     //Car class property OR member variables
     private var engineStatus:Boolean = false        //cannot access engineStatus using instance as it is private
-    var carColor: String = "White"
+    var carColor: String = color
 
     //get the engineStatus
     fun printEngineStatus(){
@@ -33,7 +34,7 @@ class Car{
         println("Car color is: $carColor")
     }
 
-   private fun changeOilFilter(doit: Boolean){
+    private fun changeOilFilter(doit: Boolean){
         if (doit)
             println("Oil filter changed")
         else
@@ -62,52 +63,11 @@ class Car{
 
 }
 
-//using Carr class in main function
-//create instances of Car class and then use it in main function
 fun main() {
-    //create instance of type Car class
-     var vellfire1:Car = Car()
-    var vellfire2:Car = Car()
-    var gls1:Car = Car()
+    //create Car instance variable
+    var car1:CarModel = CarModel("Maroon")
+    var car2:CarModel = CarModel("Silver")
 
-    //this will not work as memory has not been alloted
-    var gls2:Car
-
-    vellfire1.startEngine()
-    vellfire1.turnOffEngine()
-    vellfire1.printCarColor()
-    //change car color for vellfire1 instance
-    vellfire1.carColor = "Red"
-    vellfire1.printCarColor()
-
-    println()
-    //change car color for vellfire2 instance
-    vellfire2.carColor = "Gold"
-    vellfire2.printCarColor()
-
-    println()
-    gls1.carColor = "Pink"
-    gls1.printCarColor()
-
-    println()
-    //change engine status
-    vellfire1.carColor = "Green"
-    vellfire1.printEngineStatus()
-
-    println()
-    vellfire1.maintenance(true, air = true, coolant = true)
-    println()
-    gls1.maintenance(oil = false, air = false, coolant = true)
+    car1.printCarColor()
+    car2.printCarColor()
 }
-
-
-
-
-
-
-
-
-
-
-
-
